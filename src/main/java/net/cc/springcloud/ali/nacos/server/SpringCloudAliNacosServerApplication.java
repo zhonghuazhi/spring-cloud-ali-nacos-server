594ccb2031b19009b96d8d8b02392b09c8176df1
zhizhonghua <zhizhonghua@boco.com.cn>
2019-01-29 09:06:13 +0800

    版本初始化

---
+++ src/main/java/net/cc/springcloud/ali/nacos/server/SpringCloudAliNacosServerApplication.java
@@ -0,0 +1,14 @@
+package net.cc.springcloud.ali.nacos.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAliNacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAliNacosServerApplication.class, args);
+    }
+
+}
+
